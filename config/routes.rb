b6a84468377996eead8c3266c5a9df3ae7ff7e66
Jordan Hatch <jordan@jordanh.net>
2011-11-05 19:28:11 +0000

    Reports table added

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Crowdlobbying::Application.routes.draw do
   get "member_causes/index"
 
   resources :causes do
-    resources :members, :controller => "MemberCauses"
+    resources :members, :controller => "MemberCauses" do
+      resources :reports
+    end
   end
   
   root :to => 'causes#index'
